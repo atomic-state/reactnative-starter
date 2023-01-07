2105bfbbc34d0a0a36327d16074b5ecf268bdb0f
danybeltran <josuebeltran_2010@hotmail.com>
2023-01-06 21:49:19 -0600

    feats: - Updated template - Showcase usage of 'expo-secure-store' as persistence method with Atomic State

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import { AtomicState } from 'atomic-state'
+import { AtomicState, PersistenceStoreType } from 'atomic-state'
 import { FetcherConfig } from 'http-react-fetcher'
+
+import { getItemAsync, setItemAsync, deleteItemAsync } from 'expo-secure-store'
+const SecureStorePersistence: PersistenceStoreType = {
+  getItem: getItemAsync,
+  setItem: setItemAsync,
+  removeItem: deleteItemAsync
+}
 
 import Navigation from 'components/Navigation'
 
 export default function App() {
   return (
-    <AtomicState persistenceProvider={AsyncStorage}>
+    <AtomicState persistenceProvider={SecureStorePersistence}>
       <FetcherConfig baseUrl='https://jsonplaceholder.typicode.com'>
         <Navigation />
       </FetcherConfig>
