70d0b9ee0f50e49b1b8fe96cc96fa72b179c6f7e
danybeltran <josuebeltran_2010@hotmail.com>
2023-01-29 00:29:53 -0600

    update template

--- src/components/Home/GetDataButton.tsx
+++ src/components/Home/GetDataButton.tsx
@@ -1,31 +1,9 @@
-import { useError, useFetch, useResolve } from 'http-react'
 import { Button } from 'react-native'
 
-import { useAppAtom } from 'shared/hooks'
+import { useTodo } from 'shared/hooks'
 
 export default function GetDataButton() {
-  const timesTouched = useAppAtom('touched').value
-
-  const { reFetch } = useFetch('/todos/[id]', {
-    id: 'todo',
-    params: {
-      id: timesTouched
-    },
-    auto: false,
-    onResolve(data) {
-      console.log(data)
-    }
-  })
-
-  // Each useResolve call works as a 'useEffect' that only runs when a request completes
-  useResolve('todo', data => {
-    alert(JSON.stringify(data, null, 2))
-  })
-
-  // Same for error. Each useError call only runs once after a request fails
-  useError('todo', () => {
-    alert('An error ocurred')
-  })
+  const { reFetch } = useTodo()
 
   return <Button title='Fetch some data' onPress={reFetch} />
 }
--- src/shared/hooks.ts
+++ src/shared/hooks.ts
@@ -1,4 +1,5 @@
 import { atomProvider, filterProvider } from 'atomic-state'
+import { useFetch } from 'http-react'
 
 import { appAtoms, appFilters } from 'shared/states'
 
@@ -8,4 +9,22 @@ export const useAppFilter = filterProvider(appFilters)
 
 export function useNavigation() {
   return useAppAtom('navigation').value
+}
+
+export function useTodo() {
+  const timesTouched = useAppAtom('touched').value
+
+  return useFetch('/todos/[id]', {
+    id: 'todo',
+    params: {
+      id: timesTouched
+    },
+    auto: false,
+    onResolve(data) {
+      alert(JSON.stringify(data, null, 2))
+    },
+    onError() {
+      alert('An error ocurred')
+    }
+  })
 }
